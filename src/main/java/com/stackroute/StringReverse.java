3d0b33fc888dd336d18e07a1f0e991fd5c01e70c
NavyaSriSura <35621399+NavyaSriSura@users.noreply.github.com>
2019-07-03 17:35:28 +0530

    Update StringReverse.java

--- src/main/java/com/stackroute/StringReverse.java
+++ src/main/java/com/stackroute/StringReverse.java
@@ -2,11 +2,12 @@ package com.stackroute;
 
 public class StringReverse {
     public  String ReverseMethod(String string) {
-        if (string == null || string.isEmpty())
+        if (string == null || string.isEmpty())//incase of empty and null stirng, ask for another valid string
             return "please enter string";
         else
         {
             String reverse = "";
+            //loop for reversing the string
             for (int i = string.length() - 1; i >=0; --i) {
                 reverse = reverse+ string.charAt(i);
             }
